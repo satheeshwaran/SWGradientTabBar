20e3e27af6718859b864dfddbac6eedb939d44ab
Satheeshwaran J <satheeshwaran.j@cognizant.com>
2013-10-30 16:13:34 +0530

    Initial Commit

---
+++ SWGradientTabBar/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  SWGradientTabBar
+//
+//  Created by Satheeshwaran on 10/30/13.
+//  Copyright (c) 2013 Satheeshwaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SWGradientTabBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SWGradientTabBar
+//
+//  Created by Satheeshwaran on 10/30/13.
+//  Copyright (c) 2013 Satheeshwaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
